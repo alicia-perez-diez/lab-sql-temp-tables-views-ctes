ec9a340c0292e9712db76178383439a62b0fc12e
alicia-perez-diez <perezdiezalicia9@gmail.com>
2024-05-06 15:03:17 +0200

    V_2

--- temp_tables_views_ctes_lab.sql
+++ temp_tables_views_ctes_lab.sql
@@ -3,12 +3,15 @@ USE sakila;
 -- Step 1: Create a View
 -- create a view that summarizes rental information for each customer. The view should include the customer's ID, name, email address, and total number of rentals (rental_count).
 
+DROP VIEW rental_summary_view;
 CREATE VIEW rental_summary_view AS
-SELECT c.customer_id AS customer_id, c.first_name AS name, c.email AS email, COUNT(r.rental_id) AS num_of_rentals
+SELECT c.customer_id AS customer_id, CONCAT(c.first_name, " ", c.last_name) AS customer_name, c.email AS email, COUNT(r.rental_id) AS rental_count
 FROM customer AS c
 JOIN rental AS r
 ON c.customer_id = r.customer_id
 GROUP BY customer_id;
+
+SELECT * FROM rental_summary_view;
 
 -- Create a Temporary Table that calculates the total amount paid by each customer (total_paid).
 -- The Temporary Table should use the rental summary view created in Step 1 to join with the payment table and calculate the total amount paid by each customer.
@@ -16,9 +19,9 @@ GROUP BY customer_id;
 CREATE TEMPORARY TABLE payments_table AS
 SELECT
 	c.customer_id,
-    c.name,
+    c.customer_name,
     c.email,
-    c.num_of_rentals,
+    c.rental_count,
     SUM(p.amount) AS amount_paid 
 FROM rental_summary_view AS c
 LEFT JOIN payment AS p
@@ -32,9 +35,9 @@ SELECT * FROM payments_table;
 
 WITH customer_payment_summary AS(
 SELECT
-	c.name,
+	c.customer_name,
     c.email,
-    c.num_of_rentals,
+    c.rental_count,
     t.amount_paid
 FROM rental_summary_view AS c
 JOIN payments_table AS t
@@ -47,19 +50,19 @@ SELECT * FROM customer_payment_summary;
 
 WITH customer_payment_summary AS (
 	SELECT
-		c.name,
+		c.customer_name,
         c.email,
-        c.num_of_rentals,
+        c.rental_count,
         t.amount_paid,
-        (t.amount_paid/c.num_of_rentals) AS average_payment_per_rental
+        (t.amount_paid/c.rental_count) AS average_payment_per_rental
 FROM rental_summary_view AS c
 JOIN payments_table AS t
 ON c.customer_id = t.customer_id
 )
 SELECT
-	name,
+	customer_name,
     email,
-    num_of_rentals,
+    rental_count,
     amount_paid,
     average_payment_per_rental
 FROM customer_payment_summary;
